aeaa489c54f1c3eb0dca6bbc7f127609a00a6b9a
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2023-09-18 22:11:10 +0000

    Api Ruta

---
+++ app/Http/Controllers/RutaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRutaRequest;
+use App\Http\Requests\UpdateRutaRequest;
+use App\Models\Ruta;
+
+class RutaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRutaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ruta $ruta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRutaRequest $request, Ruta $ruta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ruta $ruta)
+    {
+        //
+    }
+}
