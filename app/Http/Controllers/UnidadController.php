d02d7655d0338e87d638dec08ea08451ccc82813
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2023-09-18 22:13:51 +0000

    Api Unidad

---
+++ app/Http/Controllers/UnidadController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUnidadRequest;
+use App\Http\Requests\UpdateUnidadRequest;
+use App\Models\Unidad;
+
+class UnidadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUnidadRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Unidad $unidad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUnidadRequest $request, Unidad $unidad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Unidad $unidad)
+    {
+        //
+    }
+}
